11d9e969e11e385f64085912754d77fe5b7004bc
Orlando <orlando.osoria@gmail.com>
2020-02-03 15:06:01 -0800

    Rest Server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,18 @@
 //===================
 
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//================
+// Base de datos
+// ==================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://orlandoosoria:BPN81MDlBecV58DV@cluster0-jv49s.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
